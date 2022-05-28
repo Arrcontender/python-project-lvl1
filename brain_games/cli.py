266da1b028a2eac5c73a7dd9ba8206fac8b1048c
Никита Дементьев <ambrosko@MacBook-Air-Nikita.local>
2022-05-28 18:59:54 +0300

    third_step

---
+++ brain_games/cli.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import prompt
+
+print('Welcome to the Brain Games!')
+
+name = prompt.string('May I have your name? ')
+
+
+def welcome_user():
+    print('Hello, ' + name + '!')
