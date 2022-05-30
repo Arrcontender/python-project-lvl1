bb41e00860ec9f02eb21692467334a8e7cefdba5
Никита Дементьев <ambrosko@MacBook-Air-Nikita.local>
2022-05-30 17:12:17 +0300

    fixed_progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,8 +4,25 @@ import random
 
 START = 'What number is missing in the progression?'
 
-
 def game_question():
+    pr_ssn_start = random.randint(0, 100)
+    pr_ssn_len = random.randint(5, 10)
+    pr_ssn_step = random.randint(2, 50)
+    pr_ssn_hide = random.randint(0, pr_ssn_len - 1)
+    pr_ssn = []
+    counter = pr_ssn_len
+    while counter > 0:
+        pr_ssn.append(str(pr_ssn_start))
+        pr_ssn_start = pr_ssn_start + pr_ssn_step
+        counter -= 1
+    hidden_num = pr_ssn[pr_ssn_hide]
+    pr_ssn.pop(pr_ssn_hide)
+    pr_ssn.insert(pr_ssn_hide, '..')
+    result = ' '.join(pr_ssn)
+    problem = hidden_num
+    return result, problem
+
+# def game_question():
     diff = random.randint(1, 5)
     first = random.randint(1, 5)
     quise = first + (random.randint(0, 9) * diff)
